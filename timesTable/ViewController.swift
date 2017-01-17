ac436bbbebb0dd00b14ecb411e53af549fd0ca39
West Kraemer <WestKraemer@gmail.com>
2017-01-17 17:01:39 -0500

    add times tables to table view, controlled by slider

--- timesTable/ViewController.swift
+++ timesTable/ViewController.swift
@@ -18,13 +18,13 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     @IBAction func sliderChanged(_ sender: Any) {
         
-        print(slider.value)
+        table.reloadData()
         
     }
     
     internal func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 1
+        return 50
         
     }
     
@@ -32,7 +32,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
         
-        cell.textLabel?.text = "Hi"
+        cell.textLabel?.text = String((Int(slider.value * 20)) * (indexPath.row + 1))
         
         return cell
     }
